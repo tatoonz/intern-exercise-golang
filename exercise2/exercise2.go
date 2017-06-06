113819d47bece242cdafd9fb7cb96ab5daa4847f
TaToonz <peerapath.t@gmail.com>
2017-06-06 21:05:24 +0700

    fix typo ref in exercise 2

--- exercise2/exercise2.go
+++ exercise2/exercise2.go
@@ -5,7 +5,7 @@
 // For example, if s = 'azcbobobegghakl',
 // then your program should return: 2
 //
-// ref: edx.org, MITx: 6.00.1x, Problem Set 1 > Problem 1
+// ref: edx.org, MITx: 6.00.1x, Problem Set 1 > Problem 2
 package exercise2
 
 // FindBob : return number of `bob` that occurs in a string
